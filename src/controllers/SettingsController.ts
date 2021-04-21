3d94c58ffa9822a2e427b9bb284a0fa2aa24078c
Joao-Couto-dev <joao.varet@gmail.com>
2021-04-21 18:15:01 +0000

    Create settings service

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,20 +1,21 @@
 import {Request, Response} from 'express';
-import { getCustomRepository } from 'typeorm';
-import SettingsRepository from '../repositories/SettingsRepository';
+import SettingsService from '../services/SettingsService';
 
 class SettingsController{
     async create(request: Request, response: Response){
         const {chat, username} = request.body;
 
-        const settingsRepository = getCustomRepository(SettingsRepository);
+        const settingsServiece = new SettingsService();
 
-        const setting = settingsRepository.create({
-            chat, username
-        });
+        try{
+            const settings = await settingsServiece.create({chat, username});
 
-        await settingsRepository.save(setting);
-
-        return response.json(setting);
+            return response.json(settings);
+        }catch(err){
+            return response.status(400).json({
+                message: err.message
+            })
+        }
     }
 }
 
